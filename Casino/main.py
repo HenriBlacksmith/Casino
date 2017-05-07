1728edaa569eef4ab13eede1a515714a9c4a575c
Henri <raid94@hotmail.fr>
2017-05-07 19:30:38 +0200

    First commit of the original code

---
+++ Casino/main.py
@@ -0,0 +1,11 @@
+# --imports
+import matplotlib.pyplot as plt
+# --class imports
+from roulette import Roulette
+# --run
+
+r = Roulette(100)
+r.plot_stats()
+plt.figure('Banque')
+plt.plot(r.martingale_simple('Noir', 10))
+plt.show()
